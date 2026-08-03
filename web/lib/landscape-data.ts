aa689fb54d16699dc8706d182549e988c335bc1f
kaykl.uz <32908844+kaykluz@users.noreply.github.com>
2026-08-03 08:20:33 +0100

    data: add Phase 1 catalogue shard 013

--- web/lib/landscape-data.ts
+++ web/lib/landscape-data.ts
@@ -18,6 +18,7 @@ import phase1009 from "../../data/landscape/shards/phase1-catalogue-009.json";
 import phase1010 from "../../data/landscape/shards/phase1-catalogue-010.json";
 import phase1011 from "../../data/landscape/shards/phase1-catalogue-011.json";
 import phase1012 from "../../data/landscape/shards/phase1-catalogue-012.json";
+import phase1013 from "../../data/landscape/shards/phase1-catalogue-013.json";
 
 export type LandscapeKind =
   | "organisation"
@@ -108,6 +109,7 @@ export const landscapeShards = [
   phase1010,
   phase1011,
   phase1012,
+  phase1013,
 ] as LandscapeShard[];
 
 export const landscapeItems = landscapeShards.flatMap((shard) => shard.items);
